3a46429cbaaf4b091df19802c26ca4fd49f03354
César <c.caycho46@gmail.com>
2020-11-21 19:21:46 -0500

    añado la estructura write

--- 1.go
+++ 1.go
@@ -1,9 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
+
+type miescritor struct {
+	contenido []byte
+	tamaño    int
+}
+
+func nuevomiescritor() *miescritor {
+	return &miescritor{}
+}
 
 func main() {
 
 	fmt.Println("Hola")
 
+}
+
+func (me *miescritor) Write(b []byte) (n int, err error) {
+
+	me.contenido = append(me.contenido, b...)
+
+	me.tamaño = len(me.contenido)
+
+	n = len(me.contenido)
+
+	if n != me.tamaño {
+		log.Fatal(err)
+	}
+
+	return
 }
